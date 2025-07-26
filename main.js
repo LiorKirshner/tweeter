33d467f66148b409748e12e650ead737603376e8
LiorKirshner <liorkk7@gmail.com>
2025-07-26 21:47:43 +0300

    tweeter module

---
+++ main.js
@@ -0,0 +1,1 @@
+import Tweeter from "./model.js";
---
+++ model.js
@@ -0,0 +1,27 @@
+const Tweeter = () => {
+  let _posts = [
+    {
+      text: "First post!",
+      id: "p1",
+      comments: [
+        { id: "c1", text: "First comment on first post!" },
+        { id: "c2", text: "Second comment on first post!!" },
+        { id: "c3", text: "Third comment on first post!!!" },
+      ],
+    },
+    {
+      text: "Aw man, I wanted to be first",
+      id: "p2",
+      comments: [
+        {
+          id: "c4",
+          text: "Don't worry second poster, you'll be first one day.",
+        },
+        { id: "c5", text: "Yeah, believe in yourself!" },
+        { id: "c6", text: "Haha second place what a joke." },
+      ],
+    },
+  ];
+  let postIdCounter = 2; // to count total posts
+  let commentIdCounter = 6; //to count total comments
+};
